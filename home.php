d59ad4c23766a545a042ffd7857a266983d379d7
Fakhri Hamdan <fakhri.hf90@gmail.com>
2023-05-07 06:25:57 +0700

    penyesuaian file

---
+++ home.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/home1.css">
+    <title>Document</title>
+</head>
+<body>
+    <main>
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Tentang Kami</a></li>
+                <li><a href="#">Kontak</a></li>
+                <li><a href="#">Marketplace</a></li>
+            </ul>
+        </nav>
+    </main>
+</body>
+</html>
